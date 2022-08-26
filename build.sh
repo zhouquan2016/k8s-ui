be377a1693e3f7b49004110d9e67cfa04269f77d
zhouquan3 <zhouquan3@yiche.com>
2022-08-26 17:52:09 +0800

    dockerfile

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-docker build ./dashboard-server/Dockerfile -t registry.cn-hangzhou.aliyuncs.com/zhqn/k8s-dashboard-server:1.0.0 && \
-docker build ./dashboard-web/Dockerfile -t registry.cn-hangzhou.aliyuncs.com/zhqn/k8s-dashboard-web:1.0.0 && \
+docker build ./dashboard-server -t registry.cn-hangzhou.aliyuncs.com/zhqn/k8s-dashboard-server:1.0.0 && \
+docker push registry.cn-hangzhou.aliyuncs.com/zhqn/k8s-dashboard-server:1.0.0 && \
+docker build ./dashboard-web -t registry.cn-hangzhou.aliyuncs.com/zhqn/k8s-dashboard-web:1.0.0 && \
+docker push registry.cn-hangzhou.aliyuncs.com/zhqn/k8s-dashboard-web:1.0.0
