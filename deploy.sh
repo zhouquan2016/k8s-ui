c06db0ec009ee9c4657bc8d8d70be05a79f90d05
zhouquan3 <zhouquan3@yiche.com>
2022-08-30 11:35:17 +0800

    build and deploy complete

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,17 @@ if [ "$2" == "rebuild" ]; then
       exit
   fi
 fi
-kubectl apply -f ./k8s.yaml && nohup kubectl port-forward --address localhost,192.168.48.136 deploy/k8s-dashdoard-server 8080:8080 &
+kubectl apply -f ./k8s.yaml && nohup kubectl port-forward --address localhost,192.168.48.136 deploy/k8s-dashboard-server 8080:8080 &
+while [ true ]; do
+    state=$(kubectl get pod | grep k8s-dashboard-web | awk '{print $3}')
+    if [ "$state" == "Running" ]; then
+      nohup kubectl port-forward --address localhost,192.168.48.136 deploy/k8s-dashboard-server 8080:8080 &
+      break
+    elif [ "$state" == "Pending"  ]; then
+      echo "wait pod running!"
+      sleep 1
+    else
+      echo "ERROR:k8s-dashboard-web pod state ${state}"
+      break
+    fi
+done
