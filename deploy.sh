a41d7e5a54ca10d02d43162d74aef919c5a10847
zhouquan3 <zhouquan3@yiche.com>
2022-08-30 11:20:43 +0800

    build and deploy complete

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+if [ $2 == "rebuild" ]; then
+  ./build.sh
+  if [ $! -ne 0  ]; then
+      exit
+  fi
+fi
+kubectl apply -f ./k8s.yaml && nohup kubectl port-forward --address localhost,192.168.48.136 deploy/k8s-dashdoard-server 8080:8080 &
